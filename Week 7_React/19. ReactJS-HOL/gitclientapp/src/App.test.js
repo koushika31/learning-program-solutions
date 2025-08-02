28743cb0139a9698ef4d27d42dd646e603d9ce66
koushika31 <141416484+koushika31@users.noreply.github.com>
2025-08-02 18:40:10 +0530

    Delete Week 7_React directory

--- Week 7_React/19. ReactJS-HOL/gitclientapp/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders GitClient heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Git repositories of User - TechieSyed/i);
-  expect(headingElement).toBeInTheDocument();
-});
--- Week 7_React/19. ReactJS-HOL/gitclientapp/src/GitClient.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-class GitClient {
-  static getRepositories(userName) {
-    return axios.get(`https://api.github.com/users/${userName}/repos`);
-  }
-}
-
-export default GitClient;
